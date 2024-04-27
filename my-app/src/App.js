6e65f1109689206f5f169e9857856fd486d6290b
dfrnti <159611720+dfrnti@users.noreply.github.com>
2024-04-27 15:52:00 +0000

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
 //import './components/Header/header-styles.css';
 //import './App.css';
-import Header from './components/Header/header.js'
+import Header from './components/Header/header.js';
+import MainList from './components/Header/main/main.js';
+import { dataForList } from './content/maindata.js';
+import Button from './components/button/button.js';
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <section>
+          <dl>
+            {dataForList.map((item) => <MainList {...item} />)}
+          </dl>
+        </section>
+        <section>
+          <h3>All buttons</h3>
+          <Button />
+        </section>
+      </main>
     </div>
   );
 }
---
+++ my-app/src/components/Header/main/main.js
@@ -0,0 +1,9 @@
+import '../main/main.css';
+export default (props) => {
+    return (
+        <>
+        <dt>{props.titel}</dt>
+        <dd>{props.description}</dd>
+        </>
+    )
+}
